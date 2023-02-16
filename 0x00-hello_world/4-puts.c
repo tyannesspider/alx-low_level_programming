2b40d52dcf66b7e5b521a1b2876f7e2f4372ae64
tyanne <tyannespider@gmail.com>
2023-02-16 09:16:53 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
-#!/bin/bash
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
 #include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
