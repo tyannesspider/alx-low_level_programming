4b4fd42bfc65f6b8b3a7e240144ae02bb244683f
tyanne <tyannespider@gmail.com>
2023-02-22 08:28:12 -0800

    task15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * mian -prints the usm of even fibonacci numbers
+ * less than 4000000
+ * Return: Nothing
+ */
+
+int main(void)
+
+{
+	int i =0;
+	long j = 1, k = 2, sum =k;
+
+	while (k + j < 4000000)
+	{
+	k += l;
+	if (k % 2 ==0)
+	sum += k;
+	++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
