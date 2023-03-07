6c1d1c2751157f4aee21b8cede2c3f55fa39a0ae
tyanne <tyannespider@gmail.com>
2023-03-07 01:45:52 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,15 +2,15 @@
 
 /**
  * _memcpy - Entry point
- * @dest: input 
- * @src: input 
+ * @dest: input
+ * @src: input
  * @n: input
  * Return: Always 0 (Sucess)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 
