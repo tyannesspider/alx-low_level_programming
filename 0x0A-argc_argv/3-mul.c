f6781a1e44449e0ed9fd6d38e8576d2e8cc76944
tyanne <tyannespider@gmail.com>
2023-03-14 05:54:44 -0700

    Task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,14 @@
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
-{	
+{
 	int n1 = 0, n2 = 0;
 
 	if (argc == 3)
 	{
 		n1 = atoi(argv[1]);
 		n2 = atoi(argv[2]);
-		printf("%d\n", n1 *n2);
+		printf("%d\n", n1 * n2);
 	}
 	else
 	{
