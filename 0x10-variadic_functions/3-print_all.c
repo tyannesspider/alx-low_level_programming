0e496546dbd196bf1c216e6c43323533c2f3cbd4
tyanne <tyannespider@gmail.com>
2023-03-23 04:11:18 -0700

    betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -24,24 +24,24 @@
 			{
 				switch (format[i])
 				{
-					case 'c':
-						printf("%s%c", sep, va_arg(list, int));
-						break;
-					case 'i':
-						printf("%s%d", sep, va_arg(list, int));
-						break;
-					case 'f':
-						printf("%s%f", sep, va_arg(list, double));
-						break;
-					case 's':
-						str = va_arg(list, char *);
-						if (!str)
-							str = "(nil)";
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
 						printf("%s%s", sep, str);
-						break;
-					default:
-						i++;
-						continue;
+					break;
+				default:
+					i++;
+					continue;
 				}
 				sep = ", ";
 				i++;
